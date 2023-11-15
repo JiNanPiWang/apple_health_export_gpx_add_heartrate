cca9de02cf97c9454783942e863fa025a080472a
sys-win11 <purep@qq.com>
2023-11-15 17:25:15 +0800

    add functions to get single data type in workout_gpx_parser.py. 加入了一些函数用来得到单一种类的数据 in workout_gpx_parser.py

--- workout_gpx_parser.py
+++ workout_gpx_parser.py
@@ -11,20 +11,41 @@ class WorkoutGpxParser:
     def __init__(self, workout_gpx_name):
         # argument: workout_gpx name
         self.workout_gpx_path = f'apple_health_export/workout-routes/{workout_gpx_name}'
+        self.tree = ET.parse(self.workout_gpx_path)
+        self.root = self.tree.getroot()
+        self.namespace = {'gpx': 'http://www.topografix.com/GPX/1/1'}
 
-    def parse_heart_rate_data(self):
-        # 解析导出.xml中的心率数据并返回
-        tree = ET.parse(self.workout_gpx_path)
-        root = tree.getroot()
-
-        namespace = {'gpx': 'http://www.topografix.com/GPX/1/1'}
-
-        for trkpt in root.findall('.//gpx:trkpt', namespace):
+    def parse_data(self):
+        for trkpt in self.root.findall('.//gpx:trkpt', self.namespace):
             lon = trkpt.attrib['lon']
             lat = trkpt.attrib['lat']
-            ele = trkpt.find('gpx:ele', namespace).text
-            time = trkpt.find('gpx:time', namespace).text
-            speed = trkpt.find('gpx:extensions/gpx:speed', namespace).text \
-                if trkpt.find('gpx:extensions/gpx:speed', namespace) is not None else "N/A"
+            ele = trkpt.find('gpx:ele', self.namespace).text
+            time = trkpt.find('gpx:time', self.namespace).text
+            speed = trkpt.find('gpx:extensions/gpx:speed', self.namespace).text \
+                if trkpt.find('gpx:extensions/gpx:speed', self.namespace) is not None else "N/A"
             yield lon, lat, ele, time, speed
 
+    def get_lon_data(self):
+        for trkpt in self.parse_data():
+            lon, _, _, _, _ = trkpt
+            yield lon
+
+    def get_lat_data(self):
+        for trkpt in self.parse_data():
+            _, lat, _, _, _ = trkpt
+            yield lat
+
+    def get_elevation_data(self):
+        for trkpt in self.parse_data():
+            _, _, ele, _, _ = trkpt
+            yield ele
+
+    def get_time_data(self):
+        for trkpt in self.parse_data():
+            _, _, _, time, _ = trkpt
+            yield time
+
+    def get_speed_data(self):
+        for trkpt in self.parse_data():
+            _, _, _, _, speed = trkpt
+            yield speed
