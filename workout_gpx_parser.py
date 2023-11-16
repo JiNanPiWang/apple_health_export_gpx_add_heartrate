72f147cfadc157305c7991930a6a5ade0ce46892
sys-win11 <purep@qq.com>
2023-11-16 11:25:34 +0800

    add self.path to workout_gpx_parser.py, can run in console, more robust. 在workout_gpx_parser.py中加入了self.path，在fleet和命令行中均可运行，增加鲁棒性

--- workout_gpx_parser.py
+++ workout_gpx_parser.py
@@ -5,16 +5,24 @@
 # including everything from an Apple Health workout export GPX file.
 
 import xml.etree.ElementTree as ET
+import os
 
 
 class WorkoutGpxParser:
     def __init__(self, workout_gpx_name):
         # argument: workout_gpx name
-        self.workout_gpx_path = f'apple_health_export/workout-routes/{workout_gpx_name}'
+        # 当前路径
+        self.path = os.path.dirname(__file__)
+
+        # gpx路径
+        self.workout_gpx_path = f'{self.path}/apple_health_export/workout-routes/{workout_gpx_name}'
+
+        # 获取GPX的信息
         self.tree = ET.parse(self.workout_gpx_path)
         self.root = self.tree.getroot()
         self.namespace = {'gpx': 'http://www.topografix.com/GPX/1/1'}
 
+    # 获取GPX的所有数据（生成器）
     def parse_data(self):
         for trkpt in self.root.findall('.//gpx:trkpt', self.namespace):
             lon = trkpt.attrib['lon']
@@ -25,6 +33,7 @@ class WorkoutGpxParser:
                 if trkpt.find('gpx:extensions/gpx:speed', self.namespace) is not None else "N/A"
             yield lon, lat, ele, time, speed
 
+    # 单独获取GPX的数据
     def get_lon_data(self):
         for trkpt in self.parse_data():
             lon, _, _, _, _ = trkpt
