7d40d3b81ad3a10c4900299cd25dee8f3fa36e54
sys-win11 <purep@qq.com>
2023-11-17 17:40:20 +0800

    Create some files for later use. 创建一些文件供后续使用

---
+++ health_export_xml_parser.py
@@ -0,0 +1,6 @@
+# health_export_xml_parser.py
+
+# 用于解析apple_health_export/导出.xml中的心率信息
+# This module contains a parser for extracting health data in apple_health_export/export.xml, including heart rate
+# from an Apple Health export XML file.
+
---
+++ main.py
@@ -0,0 +1,4 @@
+import health_export_xml_parser
+
+if __name__ == '__main__':
+    pass
