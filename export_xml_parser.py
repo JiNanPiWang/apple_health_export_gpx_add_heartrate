3550455633b4d52d824146b0db824509e855ee9b
sys-win11 <purep@qq.com>
2023-11-23 09:44:24 +0800

    Implemented the heart rate acquisition function of export_xml_parser.py and conducted unit testing on it. 实现了export_xml_parser.py的获取心率功能，并对其进行了单元测试

--- export_xml_parser.py
+++ export_xml_parser.py
@@ -9,11 +9,21 @@ import os
 import argparse
 
 
-class HealthExportXmlParser:
+class ExportXmlParser:
     def __init__(self):
         # 当前路径
         self.path = os.path.dirname(__file__)
         # xml路径
         self.health_export_xml_path = os.path.join(self.path, 'apple_health_export/export.xml')
-        
-        
+
+        # 获取xml的信息
+        with open(self.health_export_xml_path, 'r', encoding='utf-8') as xml_file:
+            self.tree = ET.parse(xml_file)
+        self.root = self.tree.getroot()
+
+    def get_heart_rate(self):
+        for record in self.root.findall('.//Record[@type="HKQuantityTypeIdentifierHeartRate"]'):
+            start_date = record.attrib.get('startDate')
+            end_date = record.attrib.get('endDate')
+            value = record.attrib.get('value')
+            yield start_date, end_date, value
---
+++ tests/test_export_xml_parser.py
@@ -0,0 +1,14 @@
+import unittest
+import datetime
+
+from export_xml_parser import ExportXmlParser
+
+
+class TestExportXmlParser(unittest.TestCase):
+    def test_get_heart_rate(self):
+        health_data = ExportXmlParser()
+        for record in health_data.get_heart_rate():
+            start_date, end_date, value = record
+            if start_date[:10] == '2019-10-03':
+                self.assertEqual(value, '70')
+                return 
