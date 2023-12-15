071be79c59aeeeeb4983fbc8440f53f925753277
sys-win11 <purep@qq.com>
2023-12-15 15:12:00 +0800

    Create merge_gpx_data.py for merging gpx. 创建merge_gpx_data.py，用于合并gpx

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
-import health_export_xml_parser
-
 if __name__ == '__main__':
     pass
---
+++ src/merge_gpx_data.py
@@ -0,0 +1,10 @@
+# merge_gpx_data.py
+# merge single_gpx_data.py's data
+import os
+
+
+class MergeGpxData:
+    def __init__(self):
+        # 当前路径
+        self.path = os.path.dirname(__file__)
+        
