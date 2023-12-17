2c27c69a4a38b755f1f40e72e4d14adffea01f25
sys-win11 <purep@qq.com>
2023-12-17 11:36:11 +0800

    Test the effect of gpxpy library. 测试gpxpy库效果

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import gpxpy
+from src.workout_gpx_parser import WorkoutGpxParser
+
 if __name__ == '__main__':
-    pass
+    gpx_file = open('apple_health_export/workout-routes/route_2023-10-18_9.30pm.gpx', 'r')
+
+    gpx = gpxpy.parse(gpx_file)
+
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                print(point.name)
--- src/merge_gpx_data.py
+++ src/merge_gpx_data.py
@@ -1,10 +1,14 @@
 # merge_gpx_data.py
 # merge single_gpx_data.py's data
+
 import os
 
 
 class MergeGpxData:
-    def __init__(self):
+    def __init__(self, file_name):
         # 当前路径
         self.path = os.path.dirname(__file__)
+        # 获取项目根目录
+        self.project_root = os.path.abspath(os.path.join(self.path, '..'))
         
+        self.file_name = file_name
