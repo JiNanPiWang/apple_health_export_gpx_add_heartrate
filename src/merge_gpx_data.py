fb6eca1f5b0811a9c7f456496082128b649c2442
sys-win11 <purep@qq.com>
2023-12-18 17:45:25 +0800

    In main.py, the gpx data is obtained through gpxpy and the file is generated through merge_gpx_data.py. 在main.py中成功实现通过gpxpy得到gpx数据，通过merge_gpx_data.py生成文件

--- src/merge_gpx_data.py
+++ src/merge_gpx_data.py
@@ -6,16 +6,14 @@ from .config import WORKOUT_ROUTES_WITH_HR
 
 
 class MergeGpxData:
-    def __init__(self, file_name):
+    def __init__(self, file_name: str, content: str):
         self.file_path = os.path.join(WORKOUT_ROUTES_WITH_HR, f'{file_name}_new.gpx')
+        self.content = content
+
         if not os.path.exists(WORKOUT_ROUTES_WITH_HR):
             os.mkdir(WORKOUT_ROUTES_WITH_HR)
 
     def save_gpx(self):
         with open(self.file_path, 'w') as f:
-            f.write(
-                """
-                <?xml version="1.0" encoding="UTF-8"?>
-<gpx creator="StravaGPX" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd" version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1" xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3">
-                """
-            )
+            f.write(self.content)
+
