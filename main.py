da30c817e41f1d9c34826c6949271f5f87af18d2
G3 <purep@qq.com>
2023-12-22 09:31:22 +0800

    Modify main.py. If need to upload strava, should provide the corresponding parameters. 修改main.py，如果需要上传strava则需要提供对应运行参数

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
+import argparse
 import os
-import json
 
 from src.merge_gpx_data import MergeGpxData
 from config.paths import WORKOUT_ROUTES
 from src.upload_gpx_to_strava import UploadGpxToStrava
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--upload-to-strava",
+        dest="upload_to_strava",
+        action="store_true",
+        help="get all keep data to gpx and download",
+    )
+    options = parser.parse_args()
+
     files = os.listdir(WORKOUT_ROUTES)
-
-    # 打印所有文件名
-    for gpx_file in files:
-        print(gpx_file)
-
+    for i, gpx_file in enumerate(files):
+        print(f'Now processing {gpx_file}')
         new_gpx = MergeGpxData(gpx_file)
         new_gpx.merge_points()
         new_gpx.save_gpx()
-        upload_to_strava = UploadGpxToStrava(new_gpx.new_file_path)
-        upload_to_strava.upload_gpx()
-
-        break
+        if options.upload_to_strava:
+            upload_to_strava = UploadGpxToStrava(new_gpx.new_file_path)
+            upload_to_strava.upload_gpx()
+        if i == 2:
+            break
