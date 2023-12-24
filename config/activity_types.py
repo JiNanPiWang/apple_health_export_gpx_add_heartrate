353126e31c93da12962eb9b7e42c697e6067f3c4
G3 <purep@qq.com>
2023-12-24 18:57:09 +0800

    Add more types for Apple to Strava sport type conversion in activity_types.py. 完善activity_types.py中Apple到Strava运动转换的种类

--- config/activity_types.py
+++ config/activity_types.py
@@ -1,3 +1,17 @@
+"""
+Strava possible values: ride, run, swim, workout, hike, walk,
+            nordicski, alpineski, backcountryski, iceskate, inlineskate,
+            kitesurf, rollerski, windsurf, workout, snowboard, snowshoe
+            Type detected from file overrides, uses athlete's default type if
+            not specified
+Apple Health Kit reference: https://developer.apple.com/documentation/healthkit/hkworkoutactivitytype
+"""
+
+
 type_trans = {'HKWorkoutActivityTypeCycling': 'Ride',
               'HKWorkoutActivityTypeRunning': 'Run',
-              'HKWorkoutActivityTypeWalking': 'Walk'}
+              'HKWorkoutActivityTypeSwimming': 'swim',
+              'HKWorkoutActivityTypeHiking': 'swim',
+              'HKWorkoutActivityTypeWalking': 'Walk',
+              'HKWorkoutActivityTypeSnowboarding': 'snowboard'
+              }
