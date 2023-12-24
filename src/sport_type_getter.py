b81d231a46c47bd6f777c85336c9cb0a988d565d
G3 <purep@qq.com>
2023-12-24 21:47:07 +0800

    sport_type_getter.py adds a method to convert the time in the file name to the universal time of the GpxDataPoint class and tests it. sport_type_getter.py增加把文件名中时间转换为GpxDataPoint类通用时间的方法，并测试

--- src/sport_type_getter.py
+++ src/sport_type_getter.py
@@ -8,11 +8,11 @@ import os
 from config.paths import WORKOUT_ROUTES
 from .export_xml_parser import ExportXmlParser
 from config.activity_types import type_trans
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from .gpx_data_point import GpxDataPoint
 
 
-def parse_date(end_date):
+def parse_date_to_filename(end_date):
     """
     Parse date from end_date to file name like
     2019-10-04 09:45:05 +0800 -> route_2019-10-04_9.45am.gpx
@@ -28,7 +28,24 @@ def parse_date(end_date):
     return file_name
 
 
+def parse_date_from_filename(file_name):
+    """
+    Parse date from file name like
+    route_2019-10-04_9.45am.gpx -> 2019-10-04 09:45:05
+    route_2019-10-03_4.05pm.gpx -> 2019-10-03 16:05:59
+    :param file_name: str
+    :return: end_date
+    """
+    _date = GpxDataPoint(time=file_name[6:-4]).datetime_origin
+    # 格式转换为utc+8，datetime比较时间会自动转换到同一时区，所以无需考虑过多内容
+    _date = _date.replace(tzinfo=timezone(timedelta(hours=8)))
+    return _date
+
+
+# TODO: 很多activity type都找不到，如果时间在startDate和endDate之间，就将中间的activity type设为这个activity type
+# 比如route_2019-10-23_12.08pm，但creation_date和end_date都是2019-10-23 12:09
 def get_sport_type(files: list[str]):
+    print('Start getting workout type for all files')
     type_dict = {}
     for record in ExportXmlParser().load_activities_type_in_dict(files):
         # record be like:
@@ -36,12 +53,17 @@ def get_sport_type(files: list[str]):
         #     'activity_type':'HKWorkoutActivityTypeCycling',
         #     'end_date':'2019-10-03 08:53:38 +0800'
         # }
-        file_name = parse_date(record['end_date'])
+        file_name = parse_date_to_filename(record['end_date'])
         try:
             strava_type = type_trans[record['activity_type']]
         except KeyError:
             print('Unsupported activity type, you can change its type in strava manually.\n'
-                  f'{file_name}\'s type set default to workout type')
+                  f'{file_name}\'s type default set to workout type')
             strava_type = 'Workout'
         type_dict[file_name] = strava_type
+
+    # TODO：在最后遍历所有文件，如果文件不在字典中，就找时间在startDate和endDate之间
+
+    print('Successfully get workout type for all files')
     return type_dict
+
--- tests/test_sport_type_getter.py
+++ tests/test_sport_type_getter.py
@@ -1,8 +1,10 @@
 import os
 import unittest
+from datetime import timezone
 
 from config.paths import WORKOUT_ROUTES
-from src.sport_type_getter import get_sport_type
+from src.sport_type_getter import *
+from src.gpx_data_point import GpxDataPoint
 
 
 class MyTestCase(unittest.TestCase):
@@ -13,6 +15,23 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(workout_type[files[0]], 'Ride')
         self.assertEqual(workout_type['route_2019-11-15_6.49am.gpx'], 'Run')
+
+    def test_get_sport_type_exist(self):
+        files = os.listdir(WORKOUT_ROUTES)
+
+        workout_type = get_sport_type(files)
+
+        for file in files:
+            self.assertTrue(file in workout_type.keys())
+
+    def test_parse_date_from_filename(self):
+        self.assertLess(parse_date_from_filename('route_2019-10-04_9.45pm.gpx'),
+                        GpxDataPoint(time='2019-10-23 12:09:05 +0800').datetime_origin)
+        self.assertEqual(parse_date_from_filename('route_2019-10-04_9.45pm.gpx'),
+                         GpxDataPoint(time='2019-10-04 21:45:00 +0800').datetime_origin)
+        # datetime比较时间会自动转换到同一时区
+        self.assertEqual(parse_date_from_filename('route_2019-10-04_9.45pm.gpx'),
+                         GpxDataPoint(time='2019-10-04 21:45:00 +0800').datetime_utc0)
 
 
 if __name__ == '__main__':
